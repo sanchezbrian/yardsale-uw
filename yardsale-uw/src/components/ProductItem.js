ba980648bba30fe6176d5c03ebd11d3eac4bdc08
Brian Sanchez <briansanchez344@gmail.com>
2022-04-26 08:44:19 -0700

    implement initial ProductItem component

---
+++ yardsale-uw/src/components/ProductItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ProductItem = props => {
+  const { product } = props;
+  return (
+    <div className=" column is-half">
+      <div className="box">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img
+                src="https://bulma.io/images/placeholders/128x128.png"
+                alt={product.shortDesc}
+              />
+            </figure>
+          </div>
+          <div className="media-content">
+            <b style={{ textTransform: "capitalize" }}>
+              {product.name}{" "}
+              <span className="tag is-primary">${product.price}</span>
+            </b>
+            <div>{product.description}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
