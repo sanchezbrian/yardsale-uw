6b9ca3fbf4a1e84b5dac9acb5fcfd40fe72b1e65
Brian Sanchez <briansanchez344@gmail.com>
2022-04-25 23:12:48 -0700

    create initial main app logic

--- yardsale-uw/src/App.js
+++ yardsale-uw/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom"
+import MainProductList from "./components/MainProductList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <Router ref={this.routerRef}>
+        <div className="App">
+          <nav
+          className='navbar container'
+          role="navigation"
+          aria-label="main navigation"
+          >
+            <div className='navbar-brand'>
+              <b className='navbar-item is-size-4'>YardSale UW</b>
+              <label
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ showMenu: !this.state.showMenu});
+                }}>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </label>
+                <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                  <Link to="/products" className="navbar-item">
+                    Products
+                  </Link>
+                </div>
+            </div>
+          </nav>
+          <Routes>
+            <Route exact path="/" component={MainProductList} />
+            <Route exact path="/products" component={MainProductList} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ yardsale-uw/src/components/MainProductList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductList() {
+    return <p>This is the Product List Page</p>
+}
