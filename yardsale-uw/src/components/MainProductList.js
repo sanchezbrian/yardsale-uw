350cfb7042e0886c24be66969b2e8a9836dfaf18
Brian Sanchez <briansanchez344@gmail.com>
2022-04-26 08:44:37 -0700

    Implement initial MainProductList component

--- yardsale-uw/src/components/MainProductList.js
+++ yardsale-uw/src/components/MainProductList.js
@@ -1,5 +1,39 @@
 import React from "react";
+import ProductItem from "./ProductItem"
+import withContext from "../withContext"
 
-export default function ProductList() {
-    return <p>This is the Product List Page</p>
-}
+const MainProductList = props => {
+    const { products } = props.context;
+
+    return (
+    <>
+      <div className="hero is-primary">
+        <div className="hero-body container">
+          <h4 className="title">Our Products</h4>
+        </div>
+      </div>
+      <br />
+      <div className="container">
+        <div className="column columns is-multiline">
+          {products && products.length ? (
+            products.map((product, index) => (
+              <ProductItem
+                product={product}
+                key={index}
+                addToCart={props.context.addToCart}
+              />
+            ))
+          ) : (
+            <div className="column">
+              <span className="title has-text-grey-light">
+                No products found!
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+    );
+};
+
+export default withContext(MainProductList);
