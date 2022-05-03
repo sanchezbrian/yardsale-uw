9c8ef605014bff8324e8dafa30b68c38c303a5f2
Abselom Sultanu <abselomsultanu4@gmail.com>
2022-05-03 00:22:52 -0700

    changed layout of homepage, still need to resize cards based on screen size

--- yardsale-uw/src/components/ProductItem.js
+++ yardsale-uw/src/components/ProductItem.js
@@ -1,30 +1,31 @@
 import React from "react";
+import axios from 'axios';
 
 const ProductItem = props => {
   const { product } = props;
   return (
-    <div className=" column is-half">
-      <div className="box">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt={product.shortDesc}
-              />
-            </figure>
-          </div>
-          <div className="media-content">
-            <b style={{ textTransform: "capitalize" }}>
-              {product.name}{" "}
-              <span className="tag is-warning">${product.price}</span>
-            </b>
-            <div>{product.description}</div>
-          </div>
-        </div>
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-square">
+      <img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
+      </img>  
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <b style={{ textTransform: "capitalize" }}>
+      {product.name}{" "}
+    <span className="tag is-warning">${product.price}</span>
+    </b> 
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
+
+
 
 export default ProductItem;
