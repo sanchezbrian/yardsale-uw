4971b2938079624506d835a27272c53f77c47196
jseifu <jsefscholarships@gmail.com>
2022-05-10 14:59:34 -0700

    update modal for product view

--- yardsale-uw/src/components/ProductItem.js
+++ yardsale-uw/src/components/ProductItem.js
@@ -1,56 +1,63 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Modal, Button } from "react-bootstrap";
 
-import React, {useState} from "react";
-import './App.css';
-
-import Modal from "react-modal";
-
-
-const ProductItem = props => {
+const ProductItem = (props) => {
   const { product } = props;
   const [isOpen, setIsOpen] = useState(false);
-
 
   function toggleModal() {
     setIsOpen(!isOpen);
   }
 
-  function sayHello(st) {
-  }
-
   return (
-
     <div class="card">
-  <div class="card-image" onClick={toggleModal}>
-    <figure class="image is-square">
-      <img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
-      </img>  
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-content">
-        <b style={{ textTransform: "capitalize" }}>
-      {product.name}{" "}
-    <span className="tag is-warning">${product.price}</span>
-    </b> 
-    </div>
-      <Modal
-          isOpen={isOpen}
-          onRequestClose={toggleModal}
-          contentLabel="My dialog"
-      >
-        <div>
-            <h2> Contact Buyer</h2>
-            <p> ProductName: {product.name} {"\n"}</p>
-            <p> Email: {product.email} {"\n"}</p>
-            <p> Description: {product.description}</p>
+      <div class="card-image" onClick={toggleModal}>
+        <figure class="image is-square">
+          <img
+            src="https://bulma.io/images/placeholders/128x128.png"
+            alt="Placeholder"
+          ></img>
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <b style={{ textTransform: "capitalize" }}>
+              {product.name}{" "}
+              <span className="tag is-warning">${product.price}</span>
+            </b>
+          </div>
+          <Modal size="xl" show={isOpen} onHide={toggleModal}>
+            <Modal.Header>
+              <Modal.Title>{product.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>
+                <img
+                  className="product-photo"
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt="Placeholder"
+                ></img>
+              </div>
+            </Modal.Body>
+            <Modal.Header>
+              <Modal.Title>Email</Modal.Title>
+            </Modal.Header>
+            <div class="box">{product.email}</div>
+            <Modal.Header>
+              <Modal.Title>Description</Modal.Title>
+            </Modal.Header>
+            <div class="box">{product.description}</div>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={toggleModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        <button onClick={toggleModal}>Close modal</button>
-      </Modal>
+      </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
