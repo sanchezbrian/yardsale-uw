bce35cdb5fd4715836c35c75fa51a2623a659ad3
sanch <brian36@uw.edu>
2022-05-23 21:32:34 -0700

    change file upload style

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -10,7 +10,7 @@ const initState = {
   name: "",
   price: "",
   description: "",
-  image:""
+  image: ""
 };
 
 class AddProduct extends Component {
@@ -57,17 +57,17 @@ class AddProduct extends Component {
       }).catch(alert);
 
       console.log(image);
-        uploadBytes(storageRef, image).then((snapshot) => {
-          console.log('Uploaded a blob or file!');
-        });
+      uploadBytes(storageRef, image).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
 
       this.setState(
-        { flash: { status: 'is-success', msg: 'Product created successfully' }}
+        { flash: { status: 'is-success', msg: 'Product created successfully' } }
       );
 
     } else {
       this.setState(
-        { flash: { status: 'is-danger', msg: 'Please enter name and price' }}
+        { flash: { status: 'is-danger', msg: 'Please enter name and price' } }
       );
     }
   };
@@ -75,81 +75,102 @@ class AddProduct extends Component {
   handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
 
   render() {
-      const {name, price, description } = this.state;
+    const { name, price, description } = this.state;
 
-      return (
-          <>
-            <div className="hero is-link">
-                <div className="hero-body container">
-                    <h4 className="title has-text-centered">Add Product</h4>
+    return (
+      <>
+        <div className="hero is-link">
+          <div className="hero-body container">
+            <h4 className="title has-text-centered">Add Product</h4>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <form onSubmit={this.save}>
+          <div className="columns is-mobile is-centered">
+            <div className="column is-one-third">
+              <div className="field">
+                <label className="label">Product Name: </label>
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label className="label">Price: </label>
+                <input
+                  className="input"
+                  type="number"
+                  name="price"
+                  value={price}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label className="label">Description: </label>
+                <textarea
+                  className="textarea"
+                  type="text"
+                  rows="2"
+                  style={{ resize: "none" }}
+                  name="description"
+                  value={description}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="field">
+                <label className="label">File Upload: </label>
+                <div class="file has-name is-small">
+                  <label class="file-label">
+                    <input
+                      class="file-input"
+                      type="file"
+                      onChange={(e) => { this.setState({ image: e.target.files[0] }) }}
+                      accept="image/png, image/gif, image/jpeg"
+                      name="resume"/>
+                      <span class="file-cta">
+                        <span class="file-icon">
+                          <i class="fas fa-upload"></i>
+                        </span>
+                        <span class="file-label">
+                          Choose a file…
+                        </span>
+                      </span>
+                      <span class="file-name">
+                        {this.state.image ? this.state.image.name : "No File Chosen"}
+                      </span>
+                  </label>
                 </div>
+                {/* <input
+                  type="file"
+                  onChange={(e) => { this.setState({ image: e.target.files[0] }) }}
+                  accept="image/png, image/gif, image/jpeg"
+                /> */}
+              </div>
+              {this.state.flash && (
+                <div className={`notification ${this.state.flash.status}`}>
+                  {this.state.flash.msg}
+                </div>
+              )}
+              <div className="field is-clearfix">
+                <button
+                  className="button is-primary is-outlined is-pulled-right"
+                  type="submit"
+                  onClick={this.save}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-            <br></br>
-            <br></br>
-            <form onSubmit={this.save}>
-                <div className="columns is-mobile is-centered">
-                    <div className="column is-one-third">
-                    <div className="field">
-                        <label className="label">Product Name: </label>
-                        <input
-                        className="input"
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={this.handleChange}
-                        required
-                        />
-                    </div>
-                    <div className="field">
-                        <label className="label">Price: </label>
-                        <input
-                        className="input"
-                        type="number"
-                        name="price"
-                        value={price}
-                        onChange={this.handleChange}
-                        required
-                        />
-                    </div>
-                    <div className="field">
-                        <label className="label">Description: </label>
-                        <textarea
-                        className="textarea"
-                        type="text"
-                        rows="2"
-                        style={{ resize: "none" }}
-                        name="description"
-                        value={description}
-                        onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="field">
-                      <label className="label">Description: </label>
-                      <input
-                        type="file"
-                        onChange={(e)=>{this.setState({image: e.target.files[0]})}}
-                        accept="image/png, image/gif, image/jpeg"
-                      />
-                    </div>
-                    {this.state.flash && (
-                        <div className={`notification ${this.state.flash.status}`}>
-                        {this.state.flash.msg}
-                        </div>
-                    )}
-                    <div className="field is-clearfix">
-                        <button
-                        className="button is-primary is-outlined is-pulled-right"
-                        type="submit"
-                        onClick={this.save}
-                        >
-                        Submit
-                        </button>
-                    </div>
-                    </div>
-                </div>
-            </form>
-          </>
-      );
+          </div>
+        </form>
+      </>
+    );
   }
 
 }
