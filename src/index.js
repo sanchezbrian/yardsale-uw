5316d6c5bd108700704d6be58b8e6c0b2226de5e
sanch <brian36@uw.edu>
2022-05-24 13:21:47 -0700

    fix test erros with CI

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -26,8 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const storage = getStorage(app);
+// const storage = getStorage(app);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -41,4 +39,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-export default storage;
